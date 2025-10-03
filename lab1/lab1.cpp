ee71b2bebfe5dfed9323cdee31a090405bf9e6f5
crosson3 <isacof38@gmail.com>
2025-10-03 18:38:30 +0200

    Update lab1.cpp

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -3,31 +3,29 @@
 
 int main()
 {
-    unsigned int x; // unsigned int т.к длина катета не может быть отрицательной и значение целочисленное
+    unsigned int x; /* длина катета не может быть отрицательной             
+                    и значение целочисленное => тип unsigned int */
     unsigned int y;
-    double z; // т.к может быть не целым числом
-    double p; // т.к может быть не целым числом
-    double size;
-    double minsize;
-    double maxsize;
+    double z; // т.к. может быть не целым числом
+    double p; // т.к. может быть не целым числом
 
-    size = pow(2, 32) - 1;
-    minsize = -((2 - pow(2, -52)) * pow(2, 1023));
-    maxsize = (2 - pow(2, -52)) * pow(2, 1023);
+    
+
     std::cout << "Первый катет = ";
     std::cin >> x;
+    std::cout << "тип - unsighed int от 0 " << "до " << UINT_MAX << std::endl;
+
 
     std::cout << "Второй катет = ";
 
     std::cin >> y;
+    std::cout << "тип - unsighed int от 0 " << "до " << UINT_MAX << std::endl;
 
 
     z = sqrt(pow(x, 2) + pow(y, 2));
     p = x + y + z;
-    std::cout << "Гипотенуза = " << z << std::endl;
-    std::cout << "Периметр = " << p << std::endl;
-    std::cout << "Тип - unsigned int, размер - 32бит, от 0 до " << size << std::endl;
-    std::cout << "Тип - double, размер - 64бит, от " << minsize << " до " << maxsize << std::endl;
+    std::cout << "Гипотенуза = " << z << "тип - double от " << DBL_MIN << "до " << DBL_MAX << std::endl;
+    std::cout << "Периметр = " << p << "тип - double от " << DBL_MIN << "до " << DBL_MAX << std::endl;
 
 
 
