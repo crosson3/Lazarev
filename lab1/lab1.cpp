97531dbc4e1203d7d91f59cce16837e5ad0fa81b
crosson3 <isacof38@gmail.com>
2025-10-08 22:01:29 +0200

    Update lab1.cpp

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <cmath>
+#include <limits>
 
 int main()
 {
@@ -8,24 +9,24 @@ int main()
     unsigned int y;
     double z; // т.к. может быть не целым числом
     double p; // т.к. может быть не целым числом
-
-    
+    unsigned int k = sizeof(unsigned int) * 8;
+    unsigned int k2 = sizeof(double) * 8;
 
     std::cout << "Первый катет = ";
     std::cin >> x;
-    std::cout << "тип - unsighed int от 0 " << "до " << UINT_MAX << std::endl;
+    std::cout << "unsighed int, занимает " << k << " бит от 0 " << "до " << std::numeric_limits<unsigned int>::max() << std::endl;
 
 
     std::cout << "Второй катет = ";
 
     std::cin >> y;
-    std::cout << "тип - unsighed int от 0 " << "до " << UINT_MAX << std::endl;
+    std::cout << "unsighed int, занимает " << k << " бит от 0 " << "до " << std::numeric_limits<unsigned int>::max() << std::endl;
 
 
     z = sqrt(pow(x, 2) + pow(y, 2));
     p = x + y + z;
-    std::cout << "Гипотенуза = " << z << " тип - double от " << DBL_MIN << "до " << DBL_MAX << std::endl;
-    std::cout << "Периметр = " << p << " тип - double от " << DBL_MIN << "до " << DBL_MAX << std::endl;
+    std::cout << "Гипотенуза = " << z << " double, занимает " << k2 << " бит от" << std::numeric_limits<double>::min() << "до " << std::numeric_limits<double>::max() << std::endl;
+    std::cout << "Периметр = " << p << " double, занимает " << k2 << " бит от" << std::numeric_limits<double>::min() << "до " << std::numeric_limits<double>::max() << std::endl;
 
 
 
