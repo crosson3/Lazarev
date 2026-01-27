2b8bef287414721e1915c8b783c24d1decf74335
crosson3 <isacof38@gmail.com>
2026-01-27 17:35:25 +0200

    Update lab4.cpp

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -1,82 +1,82 @@
-﻿#include <iostream>
+#include <iostream>
 
 int main()
 {
-	const int msize = 4;
-	int arr[msize];
-	std::cout << "введите 4 элемента массива" << std::endl;
-	for (int i = 0; i < msize; ++i) {
-		int num;
-		std::cin >> num;
-		arr[i] = num;
-	}
-	char minfirst = 9;
-	char minsecond = 9;
-	int num1 = abs(arr[0]);
-	int num2 = abs(arr[3]);
-	while (num1 > 0) {		
-		if (num1 % 10 < minfirst) {
-			minfirst = num1 % 10;
-		}
-		num1 /= 10;
-	}
-	while (num2 > 0) {		
-		if (num2 % 10 < minsecond) {
-			minsecond = num2 % 10;
-		}
-		num2 /= 10;
-	}
-	if (minfirst == minsecond) { 
-		for (int i = 0; i < msize; ++i) {
-			for (int j = i + 1; j < msize; ++j) {
-				if (arr[i] > arr[j]) {
-					std::swap(arr[i], arr[j]);
-				}
-			}
-		}
-		std::cout << "Упорядоченный массив по возрастанию" << std::endl;
-		for (int i = 0; i < msize; ++i) {
-			
-			std::cout << arr[i] << ' ';
-		}
-	}
-	else {
-		std::cout << "Условие не выполнено" << std::endl;
 
-	}
-	std::cout << std::endl;
-	const int l = 3;
-	const int w = 4;
-	int nl = 0;
-	int minsum = INT_MAX;
-	int matrix[l][w];
-	std::cout << "введите 12 элементов матрицы" << std::endl;
-	for (int i = 0; i < l; ++i) {
-		for (int j = 0; j < w; ++j) {
-			std::cin >> matrix[i][j];
-		}
-	}
-	for (int i = 0; i < l; ++i) { 
-		int sum = 0;
-		for (int j = 0; j < w; ++j) {
-			sum += matrix[i][j];
-		}
-		if (abs(sum) < minsum) {
-			minsum = abs(sum);
-			nl = i;
-		}
-	}
-	std::cout << "Номер строки подходящей по условию " << nl + 1 << std::endl;
-		
-	
-	for (int j = 0; j < w; ++j) { 
-		matrix[nl][j] = 0;
-	}
-	std::cout << "Матрица после замены " << nl + 1 << " строки на 0" << std::endl;
-	for (int i = 0; i < l; ++i) {
-		for (int j = 0; j < w; ++j) {
-			std::cout << matrix[i][j] << " ";
-		}
-		std::cout << std::endl;
-	}
+    const int msize = 7;
+    int arr[msize];
+    std::cout << "введите 7 элементов массива:" << std::endl;
+    for (int i = 0; i < msize; ++i) {
+        std::cin >> arr[i];
+    }
+
+    bool div = true;
+    for (int i = 0; i < msize; ++i) {
+        if (arr[i] % 5 != 0) {
+            div = false;
+            break;
+        }
+    }
+
+    if (div) {
+
+        for (int i = 0; i < msize - 1; ++i) {
+            for (int j = i + 1; j < msize; ++j) {
+                if (arr[i] > arr[j]) {
+                    std::swap(arr[i], arr[j]);
+                }
+            }
+        }
+        std::cout << "упорядоченный массив по возрастанию:" << std::endl;
+        for (int i = 0; i < msize; ++i) {
+            std::cout << arr[i] << ' ';
+        }
+        std::cout << std::endl;
+    }
+    else {
+        std::cout << "условие не выполнено " << std::endl;
+    }
+
+    const int rows = 3;
+    const int cols = 4;
+    int matrix[rows][cols];
+    std::cout << "введите 12 элементов матрицы: " << std::endl;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            std::cin >> matrix[i][j];
+        }
+    }
+
+    int maxNegative = 0;
+    int numCol = -1;
+    for (int j = 0; j < cols; ++j) {
+        int countNegative = 0;
+        for (int i = 0; i < rows; ++i) {
+            if (matrix[i][j] < 0) {
+                countNegative++;
+            }
+        }
+        if (countNegative > maxNegative) {
+            maxNegative = countNegative;
+            numCol = j;
+        }
+    }
+    if (numCol != -1) {
+         
+        for (int i = 0; i < rows; ++i) {
+            matrix[i][numCol] = -1;
+        }
+        std::cout << "матрица после замены столбца " << numCol + 1 << " на -1:" << std::endl;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                std::cout << matrix[i][j] << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+    else {
+        std::cout << "отрицательных нет" << std::endl;
+    }
+
+    return 0;
 }
