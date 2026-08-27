b9e1a3686016d2a457cd2ad996386a59d6ee6852
crosson3 <isacof38@gmail.com>
2026-08-27 18:55:56 +0200

    2L5

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QApplication>
-#include "registrywindow.h"
-
-int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-    RegistryWindow window;
-    window.show();
-    return app.exec();
-}
